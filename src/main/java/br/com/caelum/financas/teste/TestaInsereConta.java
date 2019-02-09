3b4d19be2d582ea50821f9dca19c4d28c302a358
Wallace <wallace.noronha@hotmail.com>
2019-02-09 12:18:29 -0200

    Adicionando a classe JPAUtil que cria o entity manager factory

--- src/main/java/br/com/caelum/financas/teste/TestaInsereConta.java
+++ src/main/java/br/com/caelum/financas/teste/TestaInsereConta.java
@@ -1,17 +1,15 @@
 package br.com.caelum.financas.teste;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
 
 public class TestaInsereConta {
 
 	public static void main(String[] args) {
 		long inicio = System.currentTimeMillis();
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("controlefinancas");
-		EntityManager manager = factory.createEntityManager();
+		EntityManager manager = new JPAUtil().getEntityManager();
 		
 		Conta conta = new Conta();
 		conta.setTitular("Bruna");
@@ -22,7 +20,7 @@ public class TestaInsereConta {
 		manager.getTransaction().begin();
 		manager.persist(conta);
 		manager.getTransaction().commit();
-		factory.close();
+		manager.close();
 		
 		System.out.println("Conta gravada com sucesso");
 		long fim = System.currentTimeMillis();
